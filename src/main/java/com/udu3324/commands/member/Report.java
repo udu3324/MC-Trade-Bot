d26cd6618907df9c0bff7d1fc000d2888d5d6a7d
udu3324 <47045986+udu3324@users.noreply.github.com>
2021-11-05 17:13:40 -0500

    Move code to a method

--- src/main/java/com/udu3324/commands/member/Report.java
+++ src/main/java/com/udu3324/commands/member/Report.java
@@ -30,12 +30,23 @@ public class Report extends ListenerAdapter {
                 Data.command + "report edd3eaa1-31db-4faf-903e-fbcfd3b501d3 - emp - https://www.youtube.com/watch?v=D1qU745zMKU \n" +
                 Data.command + "report edd3eaa131db4faf903efbcfd3b501d3 - deez nuts - https://www.youtube.com/watch?v=zYUvgxwuJbA ");
         eb.setColor(new Color(0x6F2EBB));
+
+        EmbedBuilder eb3 = new EmbedBuilder();
+        eb3.setTitle("Use this channel correctly!");
+        eb3.setDescription("This channel is for only reporting scammers. Look below for more info on how to report a scammer.\n" +
+                "```" +
+                Data.command + "report [player-ign/uuid] - [what they scammed] - [youtube link]" +
+                "```\n" +
+                "**Examples That Would Work** \n" +
+                Data.command + "report NintendoOS - dyrn - https://www.youtube.com/watch?v=dQw4w9WgXcQ \n" +
+                Data.command + "report edd3eaa1-31db-4faf-903e-fbcfd3b501d3 - emp - https://www.youtube.com/watch?v=D1qU745zMKU \n" +
+                Data.command + "report edd3eaa131db4faf903efbcfd3b501d3 - deggs - https://www.youtube.com/watch?v=zYUvgxwuJbA ");
+
         Message report = event.getMessage();
         MessageChannel channel = event.getChannel();
         if (report.getContentRaw().contains(Data.command + "report")) {
             if (report.isFromType(ChannelType.TEXT)) {
                 if (report.getChannel() == Data.report) {
-
                     //filter out messages with only report and nothing else in it
                     if (report.getContentRaw().equals(Data.command + "report")) {
                         report.reply(eb.build()).queue(message -> message.delete().queueAfter(10, TimeUnit.SECONDS));
@@ -78,32 +89,7 @@ public class Report extends ListenerAdapter {
                                             report.reply("Sorry, but this person has been already reported.").queue(message -> message.delete().queueAfter(3, TimeUnit.SECONDS));
                                             report.delete().queueAfter(500, TimeUnit.MILLISECONDS);
                                         } else {
-                                            //gets userID
-                                            String temp = String.valueOf(report.getAuthor());
-                                            int substring1 = String.valueOf(report.getAuthor()).indexOf("(") + 1;
-                                            int substring2 = String.valueOf(report.getAuthor()).length() - 1;
-                                            String userID = temp.substring(substring1, substring2);
-                                            //gets stolen/scammed for and video proof
-                                            String IGN = com.udu3324.api.IGN.find(checkForIGN);
-                                            int secondArgumentSeparator = input.indexOf(" - ", firstArgumentSeparator + 3);
-                                            String scammedFor = input.substring(firstArgumentSeparator + 3, secondArgumentSeparator);
-                                            String videoLink = input.substring(secondArgumentSeparator + 3);
-
-                                            EmbedBuilder eb2 = new EmbedBuilder();
-                                            eb2.setAuthor(IGN + " (report waiting to be confirmed)", null, null);
-                                            eb2.setDescription("[" + UUID + "](https://namemc.com/profile/" + checkForIGN + ")\n" + "This person is a **REPORTED** scammer! Don't trade with them unless you know what you're doing!");
-                                            eb2.setThumbnail("https://crafatar.com/renders/body/" + UUID + "?overlay");
-                                            eb2.addField("Scammed For", scammedFor, false);
-                                            eb2.addField("Proof", videoLink, false);
-                                            eb2.addField("Reported By", "<@" + userID + ">", false);
-                                            eb2.setColor(new Color(0x181818));
-                                            eb2.setFooter("To report another scammer, do \"" + Data.command + "report [player-ign/uuid] - [what they scammed] - [youtube link]\" again.");
-                                            //this is optional and is to get players opinions
-                                            report.reply(eb2.build()).queue(message -> {
-                                                message.addReaction("\u2705").queue();
-                                                message.addReaction("\u274E").queue();
-                                            });
-                                            CreateReport.create(UUID, scammedFor, videoLink);
+                                            createReport(report, input, checkForIGN, firstArgumentSeparator, UUID);
                                         }
                                     }
                                 }
@@ -128,18 +114,37 @@ public class Report extends ListenerAdapter {
             }
         } else if (report.getChannel() == Data.report && !event.getAuthor().isBot() && !isStaffMember && !report.getContentRaw().contains(Data.command + "reject") && !report.getContentRaw().contains(Data.command + "accept")) {
             channel.sendMessage("Hey! You aren't allowed to talk here unless if you're submitting a scammer report.").queue(message -> message.delete().queueAfter(10, TimeUnit.SECONDS));
-            EmbedBuilder eb3 = new EmbedBuilder();
-            eb3.setTitle("Use this channel correctly!");
-            eb3.setDescription("This channel is for only reporting scammers. Look below for more info on how to report a scammer.\n" +
-                    "```" +
-                    Data.command + "report [player-ign/uuid] - [what they scammed] - [youtube link]" +
-                    "```\n" +
-                    "**Examples That Would Work** \n" +
-                    Data.command + "report NintendoOS - dyrn - https://www.youtube.com/watch?v=dQw4w9WgXcQ \n" +
-                    Data.command + "report edd3eaa1-31db-4faf-903e-fbcfd3b501d3 - emp - https://www.youtube.com/watch?v=D1qU745zMKU \n" +
-                    Data.command + "report edd3eaa131db4faf903efbcfd3b501d3 - deggs - https://www.youtube.com/watch?v=zYUvgxwuJbA ");
             channel.sendMessage(eb3.build()).queue(message -> message.delete().queueAfter(10, TimeUnit.SECONDS));
             report.delete().queueAfter(500, TimeUnit.MILLISECONDS);
         }
+    }
+
+    public static void createReport(Message report, String input, String checkForIGN, int firstArgumentSeparator, String UUID) throws Exception {
+        //gets userID
+        String temp = String.valueOf(report.getAuthor());
+        int substring1 = String.valueOf(report.getAuthor()).indexOf("(") + 1;
+        int substring2 = String.valueOf(report.getAuthor()).length() - 1;
+        String userID = temp.substring(substring1, substring2);
+        //gets stolen/scammed for and video proof
+        String IGN = com.udu3324.api.IGN.find(checkForIGN);
+        int secondArgumentSeparator = input.indexOf(" - ", firstArgumentSeparator + 3);
+        String scammedFor = input.substring(firstArgumentSeparator + 3, secondArgumentSeparator);
+        String videoLink = input.substring(secondArgumentSeparator + 3);
+
+        EmbedBuilder eb2 = new EmbedBuilder();
+        eb2.setAuthor(IGN + " (report waiting to be confirmed)", null, null);
+        eb2.setDescription("[" + UUID + "](https://namemc.com/profile/" + checkForIGN + ")\n" + "This person is a **REPORTED** scammer! Don't trade with them unless you know what you're doing!");
+        eb2.setThumbnail("https://crafatar.com/renders/body/" + UUID + "?overlay");
+        eb2.addField("Scammed For", scammedFor, false);
+        eb2.addField("Proof", videoLink, false);
+        eb2.addField("Reported By", "<@" + userID + ">", false);
+        eb2.setColor(new Color(0x181818));
+        eb2.setFooter("To report another scammer, do \"" + Data.command + "report [player-ign/uuid] - [what they scammed] - [youtube link]\" again.");
+        //this is optional and is to get players opinions
+        report.reply(eb2.build()).queue(message -> {
+            message.addReaction("\u2705").queue();
+            message.addReaction("\u274E").queue();
+        });
+        CreateReport.create(UUID, scammedFor, videoLink);
     }
 }
